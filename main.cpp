605a810f5fdff0197e4726651cfef943550c60f2
Keivan-sf <keyvan0082@gmail.com>
2023-10-28 12:38:55 +0330

    Implement normal insertion sort in c++

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    int a = 12;
+    int array[5] = {99, 23, 3, -2, 5};
+    int n = 5;
     cout << "hello" << endl;
+    for (int i = 1; i<5; i++) {
+        int key = array[i];
+        int j = i - 1;
+        while (j > -1 && array[j] > key) {
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j + 1] = key;
+    }
+
+    for (int i = 0; i < 5; i++) {
+        cout<< array[i] << " ";
+    }
 }
