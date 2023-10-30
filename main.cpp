895c2d63b9fc3e8550dd402a6c76a9b81a774401
Keivan-sf <keyvan0082@gmail.com>
2023-10-30 19:09:41 +0330

    Implement insertion sort with middle tracking

--- main.cpp
+++ main.cpp
@@ -16,66 +16,70 @@ void print_linked_list(node* head) {
     cout <<"\n";
 }
 
-/* int main222() { */
-/*     int array[5] = {99, 23, 3, -2, -1}; */
-/*     int n = 5; */
-/*     int l = 1, r = 1; */
-/*     node *last = new(node); */
-/*     node *head = last; */
-/*     node *mid = last; */
-/*  */
-/*     last->value = array[0]; */
-/*     for(int i =1; i<n; i++) { */
-/*         int target = array[i]; */
-/*         node *newNode = new(node); */
-/*         newNode -> value = target; */
-/*         if (target > mid->value) { */
-/*             newNode -> prev = last; */
-/*             last -> next = newNode; */
-/*             node *current = last; */
-/*             node *placement = head; */
-/*             last = newNode; */
-/*             while(true) { */
-/*                 if(current -> value <= target) { */
-/*                     placement = current -> next; */
-/*                     break; */
-/*                 } else if(!(current -> prev)) { */
-/*                     current -> next -> value = current -> value; */
-/*                     break; */
-/*                 } else { */
-/*                     current -> next -> value = current -> value; */
-/*                     current = current -> prev; */
-/*                 } */
-/*             } */
-/*             placement->value = target; */
-/*         } else { */
-/*             newNode -> prev = last; */
-/*             last -> next = newNode; */
-/*             node *current = last; */
-/*             node *placement = head; */
-/*             last = newNode; */
-/*             while(true) { */
-/*                 if(current -> value <= target) { */
-/*                     placement = current -> next; */
-/*                     break; */
-/*                 } else if(!(current -> prev)) { */
-/*                     current -> next -> value = current -> value; */
-/*                     break; */
-/*                 } else { */
-/*                     current -> next -> value = current -> value; */
-/*                     current = current -> prev; */
-/*                 } */
-/*             } */
-/*             placement->value = target; */
-/*         } */
-/*         print_linked_list(head); */
-/*         cout << "And the middle is = " << mid->value << "\n"; */
-/*     } */
-/*     print_linked_list(head); */
-/*     return 0; */
-/* } */
-
 int main() {
+    int array[9] = {100, 80, 81, 2, 99, 23, 3, -2, -1};
+    int n = 9;
+    int l = 0, r = 0;
+    node *last = new(node);
+    node *head = last;
+    node *mid = last;
+
+    last->value = array[0];
+    for(int i =1; i<n; i++) {
+        int target = array[i];
+        node *newNode = new(node);
+        newNode -> value = target;
+        if (target > mid->value) {
+            newNode -> prev = last;
+            last -> next = newNode;
+            node *current = last;
+            node *placement = head;
+            last = newNode;
+            while(true) {
+                if(current -> value <= target) {
+                    placement = current -> next;
+                    break;
+                } else if(!(current -> prev)) {
+                    current -> next -> value = current -> value;
+                    break;
+                } else {
+                    current -> next -> value = current -> value;
+                    current = current -> prev;
+                }
+            }
+            placement->value = target;
+            if(r%2 == 0 && mid->next) mid = mid ->next;
+            r++;
+        } else {
+            newNode -> next = head;
+            head -> prev = newNode;
+            node *current = head;
+            node *placement = last;
+            head = newNode;
+            while(true) {
+                if(current -> value > target) {
+                    placement = current -> prev;
+                    break;
+                } else if(!(current -> next)) {
+                    current -> prev -> value = current -> value;
+                    break;
+                } else {
+                    current -> prev -> value = current -> value;
+                    current = current -> next;
+                }
+            }
+            placement->value = target;
+            if(l%2 == 0 && mid->prev) mid = mid ->prev;
+            l++;
+        }
+        print_linked_list(head);
+        cout << "And the middle is = " << mid->value << "\n";
+    }
+    print_linked_list(head);
+    return 0;
+}
+
+int main_reversed() {
     int array[5] = {99, 23, 3, -2, -1};
     int n = 5;
     node *last = new(node);
