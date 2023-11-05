8c6c0f8f582d2da86fb553aee190e6b37257d9c0
Keivan-sf <keyvan0082@gmail.com>
2023-11-05 18:16:44 +0330

    Move normal isnertion sorts to a separate file

--- main.cpp
+++ main.cpp
@@ -5,65 +5,9 @@
 #include <ctime>
 #include "middle-tracking.h"
 #include "linkedlist.h"
+#include "normal-insertion-sort.h"
 
 using namespace std;
-
-node* insertion_sort_with_linked_list(int *array, int n) {
-    node *last = new(node);
-    node *head = last;
-    last->value = array[0];
-    for(int i =1; i<n; i++) {
-        int target = array[i];
-        node *newNode = new(node);
-        newNode -> value = target;
-        newNode -> prev = last;
-        last -> next = newNode;
-        node *current = last;
-        node *placement = head;
-        last = newNode;
-        while(true) {
-            if(current -> value <= target) {
-                placement = current -> next;
-                break;
-            } else if(!(current -> prev)) {
-                current -> next -> value = current -> value;
-                break;
-            } else {
-                current -> next -> value = current -> value;
-                current = current -> prev;
-            }
-        }
-        placement->value = target;
-    }
-    return head;
-}
-
-int insertion_sort_out_of_place(int *array, int n) {
-    int output_array[n];
-    for (int i = 1; i<n; i++) {
-        int key = array[i];
-        output_array[i] = array[i];
-        int j = i - 1;
-        while (j > -1 && output_array[j] > key) {
-            output_array[j + 1] = output_array[j];
-            j--;
-        }
-        output_array[j + 1] = key;
-    }
-    return 0;
-}
-
-void insertion_sort(int *array, int n) {
-    for (int i = 1; i<n; i++) {
-        int key = array[i];
-        int j = i - 1;
-        while (j > -1 && array[j] > key) {
-            array[j + 1] = array[j];
-            j--;
-        }
-        array[j + 1] = key;
-    }
-}
 
 long date_time_in_ms() {
     return chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch()).count();
