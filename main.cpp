ed6ab2ea165c02ff3e0c060e8edb1f8897c69a53
Keivan-sf <keyvan0082@gmail.com>
2023-11-06 17:31:15 +0330

    Clean up output

--- main.cpp
+++ main.cpp
@@ -6,34 +6,36 @@
 using namespace std;
 
 int main() {
+    cout << "Processing...\n\n";
     int array[30000];
     int n = 30000;
     int start, end;
     srand(date_time_in_ms() % 11);
 
     start = date_time_in_ms();
-    /* populate_with_perfect_middletracking_input(array, n); */
     populate_with_perfect_insertionsort_input(array,n);
-    /* populate_with_random_numbers(array, n, 0, 1000000); */
     end = date_time_in_ms();
-    cout << "random number geneartion finished in: " << end - start << " ms" << endl;
+    long random_number_generation_time = end - start;
 
     start = date_time_in_ms();
     node* head = insertion_sort_with_middle_tracking(array, n);
     end = date_time_in_ms();
-    cout << "middle tracking finished in: " << end - start << " ms" << endl;
-
-    int middle_tracking_time = end - start;
+    long middle_tracking_time = end - start;
 
     start = date_time_in_ms();
     convert_linked_list_to_array(head,n);
     end = date_time_in_ms();
-    cout << "middle tracking with array output finished in: " << middle_tracking_time + (end - start)<< " ms" << endl;
+    long middle_tracking_array_convertion_time = middle_tracking_time + (end - start);
 
     start = date_time_in_ms();
     insertion_sort(array, n);
     end = date_time_in_ms();
-    cout << "normal insertion sort finished in: " << end - start << " ms" << endl;
+    long normal_insertion_sort_time = end - start;
+
+    cout << "Normal insertion-sort " << normal_insertion_sort_time << " ms\n\n";
+    cout << "Middle-tracking: " << middle_tracking_time << " ms\n\n";
+    cout << "Middle-tracking with array conversion: " << middle_tracking_array_convertion_time << " ms\n\n";
+    cout << "Input generation: " << random_number_generation_time << " ms\n";
 
     return 0;
 }
